3cc5b10ea740241ce9816cfbe65a19cfce7b54e3
David <aravol.amakiir@gmail.com>
2015-04-19 09:31:10 -0600

    Updated comments

--- Jacinth/Components/InheritComponentKeyAttribute.cs
+++ Jacinth/Components/InheritComponentKeyAttribute.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace Jacinth.Components
 {
+    /// <summary>
+    /// Attribute to indicate the marked comopnent should use it's base class's ComponentTypeKey.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public class InheritComponentKeyAttribute : Attribute
     {
+        // Nothing to here but exist
     }
 }
